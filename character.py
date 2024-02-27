68570b30fb1e7c2918eba1a56d1668277dc3c035
Rafael Cruz <rcas2009@outlook.com>
2024-02-27 07:37:14 -0300

    Creating character class

---
+++ character.py
@@ -0,0 +1,11 @@
+class Character:
+    # Variables created before the __init__ method are class-level variables, shared across all instances of the class
+    def __init__(self, name: str, health: float, damage: float) -> None:
+        self.health = health
+        self.name = name
+        self.max_health = health
+        self.damage = damage
+
+    def attack(self, target) -> None:
+        target.health -= self.damage
+        target.health = max(target.hp, 0)
