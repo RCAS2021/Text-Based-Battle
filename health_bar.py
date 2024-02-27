adff26f586d90680f1817120fb765a22d146d31f
Rafael Cruz <rcas2009@outlook.com>
2024-02-27 09:09:24 -0300

    Commenting on health_bar funcitionality

--- health_bar.py
+++ health_bar.py
@@ -4,9 +4,13 @@ import os
 os.system("")
 
 class HealthBar():
+    # Symbol for remaining health
     symbol_remaining: str = chr(9608)
+    # Symbol for lost health
     symbol_lost: str = "_"
+    # Barrier
     barrier: str = "|"
+    # Possible colors
     colors: dict = {"red": "\033[91m",
                     "purple": "\033[95m",
                     "blue": "\033[34m",
@@ -17,19 +21,23 @@ class HealthBar():
                     "default": "\033[0m"
                     }
 
+    # Constructor method
     def __init__(self, entity, length:int = 20, is_colored: bool = True, color: str = "") -> None:
         # Entity values
         self.entity = entity
         self.current_value = entity.health
         self.max_value = entity.max_health
 
+        # Health Bar values
         self.length = length
         self.is_colored = is_colored
         self.color = self.colors.get(color, self.colors.get("default"))
 
+    # Updates current value to the entity's health
     def update(self) -> None:
         self.current_value = self.entity.health
 
+    # Draws the health bar
     def draw(self) -> None:
         remaining_bars = round(self.current_value / self.max_value * self.length)
         lost_bars = self.length - remaining_bars
