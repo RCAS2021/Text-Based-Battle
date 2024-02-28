ed1f225e97a4363c3f4e24ac5a555d380b837680
Rafael Cruz <rcas2009@outlook.com>
2024-02-28 20:03:19 -0300

    feat: add combat order difference by weapon_type

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from weapon import *
 def run() -> None:
     # Instanciating the class, getting two objects
     hero = Hero(name="Hero", health=100, strength=5, weapon=iron_sword, secondary_weapon=short_bow)
-    enemy = Enemy(name="Enemy", health=25, strength=3, weapon=fists)
+    enemy = Enemy(name="Enemy", health=25, strength=3, weapon=short_bow)
 
     while True:
         hero.health_bar.draw()
@@ -15,15 +15,23 @@ def run() -> None:
         action = input("\nSelect your action: \nAttack - A\nBlock - B\nSwap Weapon- S\n")
         os.system('cls')
         if action.upper() == "A":
-            hero.attack(enemy)
+            if (hero.weapon.weapon_type == "Melee" and enemy.weapon.weapon_type == "Ranged"):
+                enemy.attack(hero)
+                hero.attack(enemy)
+            # Possible (hero x enemy): melee x melee, ranged x ranged, throw x melee, throw x ranged, ranged x mele
+            else:
+                hero.attack(enemy)
+                enemy.attack(hero)
+
         elif action.upper() == "B":
             hero.block()
+            enemy.attack(hero)
         elif action.upper() == "S":
             hero.swap(item=hero.weapon)
+            enemy.attack(hero)
         else:
             print("Wrong command")
 
-        enemy.attack(hero)
         # Printing health bar
         hero.health_bar.draw()
         enemy.health_bar.draw()
